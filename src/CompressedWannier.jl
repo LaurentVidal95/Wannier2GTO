e177e15811efb99aa3f245d2f3243ed7f50339a6
LaurentVidal95 <laurent.vida@gmail.com>
2023-06-14 16:18:05 +0200

    Added a routine to read the compressed wannier from file.

--- src/CompressedWannier.jl
+++ src/CompressedWannier.jl
@@ -7,7 +7,7 @@ mutable struct CompressedWannier{TR<:Real, TC<:Complex}
     center          :: Vector{TR}
     # Each MO is a name tuple with a list of coeffs and corresponding SAGTOs
     basis_functions :: Vector{BasisFunction}
-    coefficients    :: Vector{TC}
+    coefficients    :: Vector{TC} # TODO change to TR...
     # Optim related
     residual        ::AbstractArray{TC}
     error           ::TR
@@ -24,6 +24,9 @@ function pol_to_arrays(pol::Polynomial)
     exps, coeffs
 end
 
+@inline function (Wc::CompressedWannier)(basis_supercell::PlaneWaveBasis)
+    Wc.coefficients'*[Φ(basis_supercell) for Φ in Wc.basis_functions]
+end
 """
 TODO: doc
 Store the compressed wannier. Residual is not stored as it can be recomputed easily.
@@ -50,8 +53,26 @@ function store(Wc::CompressedWannier; file="compressed_wannier.json")
     nothing
 end
 
-# function CompressedWannier(basis_supercell, wannier, center, file)
-#     # TODO: complete the function ! In particular have to parse JSON arrays as BasisFunction
-#     data = open(JSON3.read, file)
-#     basis_functions = [BasisFunction(ComplexF64.(Φ.coeffs), Φ.SAGTO) for Φ in test.basis_functions]
-# end
+function CompressedWannier(basis_supercell, file)
+    # TODO: complete the function ! In particular have to parse JSON arrays as BasisFunction
+    data = open(JSON3.read, file)
+    basis_functions = BasisFunction[]
+    TR = eltype(basis_supercell)
+    TC = complex(TR)
+
+    # Parse JSON3 arrays as BasisFunctions
+    for Φ in data.basis_functions
+        SAGTOs = []
+        for X in Φ.SAGTO
+            exps = Tuple.(X[1])
+            coeffs = Vector(X[2])
+            center = Vector(X[3])
+            spread = X[4]
+            push!(SAGTOs, GaussianPolynomial(exps, coeffs, center, spread))
+        end
+        coeffs = TC.(Φ.coeffs)
+        push!(basis_functions, BasisFunction(coeffs, SAGTOs))
+    end
+    CompressedWannier(basis_supercell, TC[], TR[], basis_functions, TC.(data.coefficients),
+                      TC[], data.error, data.error_norm)
+end
