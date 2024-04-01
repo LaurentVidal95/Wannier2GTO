de094e35a5a1be6f9aef3d851d4b738d599f08a2
LaurentVidal95 <laurent.vida@gmail.com>
2024-04-01 14:08:40 +0200

    Snapshot before changing to single spread.

--- src/compression/wannier_preprocessing.jl
+++ src/compression/wannier_preprocessing.jl
@@ -30,15 +30,16 @@ function find_π_bond_axis(basis_supercell::PlaneWaveBasis, wannier, wannier_cen
                           debug_sign=1)
 
     @info "Computing the π_bond axis in polar coordinates" 
-    s_orb(α, ζ) = GaussianPolynomial([(0,0,1)], [1.], α, ζ)
+    s_orb(α, ζ) = GaussianPolynomial([(0,0,1)], [1.], α, exp(ζ))
     α(λ, θ) = polar_to_cartesian_coords(wannier_center, λ, θ)
 
     # Try to fit a s-like orbital to the part of the wannier function located
     # on the π-bond axis.
     res = optimize(X->norm(wannier + debug_sign*s_orb(α(X[1],X[2]), X[3])(basis_supercell)),
                    # Guess roughly close to wanted axis by experience.
-                   [1/3., -1/2, 2] .+ wannier_center,
-                   ConjugateGradient(linesearch=BackTracking(order=3)), Optim.Options(show_trace=false))
+                   [1/3., -1/2, log(2)] .+ wannier_center,
+                   ConjugateGradient(linesearch=BackTracking(order=3)),
+                   Optim.Options(show_trace=false))
     r, θ = res.minimizer[1:2]
 
     # If r is approximately zero the hack failed or the s orbitals have wrong signs.
@@ -81,50 +82,3 @@ function prepare_for_compression(wann_model, scfres; wannier_manual_selection=no
 
     (; basis_supercell, wannier, center, π_bond)
 end
-
-#
-# Old code adapted to the use of Wannier90
-#
-
-# using DelimitedFiles
-#
-# Extract Wannier90 output
-# function read_w90_output_mat(file, n_bands_tot)
-#     Uks = readdlm(file); num_kpts, num_ψout, num_ψin = Uks[2,1:3];
-#     len_Uk = num_ψin*num_ψout
-
-#     list_Uk = []
-#     for k in 1:num_kpts
-#         i_Uk = 4 + (k-1)*(len_Uk+1)
-#         Uk = reshape([x+im*y for (x,y) in eachrow(Uks[i_Uk:i_Uk+(len_Uk-1), 1:2])],
-#                      num_ψin, num_ψout)
-#         (norm(Uk'Uk - I) > 1e-8) && (error("U matrix has non-orthogonal columns"))
-#         # Complete U matrices by zero to ease the writing of `apply_U_matrices`
-#         (n_bands_tot ≠ num_ψin) &&
-#             ( Uk = vcat(Uk, zeros(ComplexF64, n_bands_tot - num_ψin, num_ψout)) )
-#         push!(list_Uk, Uk)
-#     end
-#     list_Uk
-# end
-
-# function apply_U_matrices_w90(file, ψ)
-#     # read .mat file
-#     n_bands_tot = size(ψ[1], 2)
-#     num_kpts = length(ψ)
-#     Uks = read_w90_output_mat(file, n_bands_tot)
-#     [ψ[k]*Uks[k] for k in 1:num_kpts]
-# end
-
-# @doc raw"""
-# Apply wannierization U matrices to return Wannier functions as a table ``W[k][G,n]``
-# The prefix in argument is the one returned by the `run_wannier90` routine above.
-# As in the wannierization process, the scfres has to be unfolded.
-# """
-# function extract_wannier_functions_w90(prefix::String, scfres::NamedTuple)
-#     scfres_unfold = DFTK.unfold_bz(scfres); ψ_unfold = scfres_unfold.ψ
-#     # Apply disentanglement if needed
-#     isfile("$(prefix)_u_dis.mat") && (ψ_unfold =
-#                        apply_U_matrices_w90("$(prefix)_u_dis.mat", ψ_unfold))
-#     # Construct Wannier functions
-#     apply_U_matrices_w90("$(prefix)_u.mat", ψ_unfold)
-# end
